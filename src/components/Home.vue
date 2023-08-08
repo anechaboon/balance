051f4a684677848a72fb3bf9290a0c5ab711cdb9
anechaBoon <developer05.gosaas@gmail.com>
2023-08-08 13:49:12 +0700

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <v-app>
+    <v-main>
+      <!-- Your main content goes here -->
+
+      <!-- Bottom Navigation -->
+      <v-bottom-navigation v-model="activeLink">
+        <v-btn v-for="(item, index) in links" :key="index" @click="activeLink = item.link">
+          <v-icon>{{ item.icon }}</v-icon>
+          {{ item.text }}
+        </v-btn>
+      </v-bottom-navigation>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeLink: 'home',
+      links: [
+        { text: 'Home', icon: 'mdi-home', link: 'home' },
+        { text: 'Favorites', icon: 'mdi-heart', link: 'favorites' },
+        { text: 'Settings', icon: 'mdi-cog', link: 'settings' },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+/* Add your custom styles here */
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <Home />
+</template>
+
+<script setup>
+  import Home from '@/components/Home.vue'
+</script>
